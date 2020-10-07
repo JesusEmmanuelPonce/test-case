e1564674ddf528330814d69bf4e7ace0e7e2c7a7
Emmanuel Ponce <jesus.ponce.supernova@gmail.com>
2020-10-06 20:09:51 -0500

    Api added

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
         <el-input v-model="searchInput" />
       </div>
       <div class="col-12 col-md-3">
-        <button @click="dialogVisible = true" class="btn">
+        <button @click="dialogAdd = true" class="btn">
           Agregar
         </button>
         <el-button @click="showUSD">
@@ -16,7 +16,7 @@
     <div class="row mt-5 justify-content-center">
       <div class="col-12">
         <span>
-          Total de empleados {{ employees.length }}
+          Total de empleados {{ totalEmployees.length }}
         </span>
         <table class="mt-2 table table-striped table-hover">
           <thead>
@@ -39,7 +39,7 @@
             </tr>
           </thead>
           <tbody>
-            <tr v-for="(c, index) in displayCategories" :key="index">
+            <tr v-for="(c, index) in displayEmployees" :key="index">
               <td>
                 {{ c.name }}
               </td>
@@ -53,16 +53,17 @@
                 {{ c.enterprise }}
               </td>
               <td>
-                <el-button @click="edit(index)" type="primary" icon="el-icon-edit" circle />
-                <el-button @click="remove(c.id)" type="danger" icon="el-icon-delete" circle />
+                <el-button @click="edit(c._id)" type="primary" icon="el-icon-edit" circle />
+                <el-button @click="remove(c._id)" type="danger" icon="el-icon-delete" circle />
               </td>
             </tr>
           </tbody>
         </table>
       </div>
     </div>
+    <!-- Add -->
     <el-dialog
-      :visible.sync="dialogVisible"
+      :visible.sync="dialogAdd"
     >
       <el-form :model="ruleForm" :rules="rules" ref="ruleForm" >
         <el-form-item prop="name">
@@ -91,18 +92,44 @@
         </button>
       </span>
     </el-dialog>
+    <!-- Edit -->
+    <el-dialog
+      :visible.sync="dialogEdit"
+    >
+      <el-form >
+        <el-form-item>
+          <span>
+            Nombre
+          </span>
+          <el-input v-model="editEmployees.name" />
+        </el-form-item>
+        <el-form-item>
+          <span>
+            Salario
+          </span>
+          <el-input type="number" v-model="editEmployees.salary" />
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button type="danger">Cancel</el-button>
+        <button type="primary" @click="editSubmit(editEmployees._id)" class="btn">
+          Editar
+        </button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
 <script>
-import Vue from 'vue'
-import uuid from 'uuid/dist/v4'
+import axios from 'axios'
 export default {
   data () {
     return {
-      dialogVisible: false,
+      totalEmployees: [],
+      editEmployees: [],
+      dialogAdd: false,
       dialogEdit: false,
-       ruleForm: {
+      ruleForm: {
           id: '',
           name: '',
           salary: '',
@@ -121,27 +148,42 @@ export default {
           ],
         },
       searchInput: '',
-      employees: [],
-      editEmployees: [],
       salaryUSD: false  
     }
   },
   computed: {
-    displayCategories () {
-      const employees = this.employees
+    displayEmployees () {
+      const employees = this.totalEmployees
       const searchInput = this.searchInput
       return employees.filter(c => {
         return c.name.toLowerCase().includes(searchInput.toLowerCase()) || c.name.toUpperCase().includes(searchInput.toUpperCase()) && c.enterprise.toLowerCase().includes(searchInput.toLowerCase()) || c.enterprise.toUpperCase().includes(searchInput.toUpperCase())
       })
     }
   },
+  created(){
+    this.list()
+  },
   methods: {
+    list () {
+      axios.get('employee')
+        .then(response => {
+          this.totalEmployees = response.data
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    },
     submitForm(formName) {
       this.$refs[formName].validate((valid) => {
         if (valid) {
-          this.ruleForm.id = uuid()
-          this.employees.push(Vue.util.extend({}, this.ruleForm))
-          this.resetForm(formName)
+          axios.post('employee/add', this.ruleForm)
+            .then(() => {
+              this.resetForm(formName)
+              this.list()
+            })
+            .catch(error => {
+              console.log(error)
+            })
         } else {
           console.log('error submit!!')
           return false
@@ -149,7 +191,7 @@ export default {
       })
     },
     resetForm(formName) {
-      this.dialogVisible = false
+      this.dialogAdd = false
       this.$refs[formName].resetFields();
     },
     currency (amount) {
@@ -168,14 +210,38 @@ export default {
     },
     edit(id) {
       this.dialogEdit = true
-      console.log(id)
-      let result = this.employees.filter(c => c.id === id)
-      result = JSON.stringify(result)
-      this.editEmployees = JSON.parse(result)
-      console.log(this.editEmployees)
+      axios.get(`employee/${id}`)
+        .then(response => {
+          this.editEmployees = response.data
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    },
+    editSubmit(id){
+      const name = this.editEmployees.name
+      const salary = this.editEmployees.salary
+      const employeeObj = {
+        name,
+        salary
+      }
+      axios.put(`employee/update/${id}`, employeeObj )
+        .then(() => {
+          this.dialogEdit = false
+          this.list()
+        })
+        .catch(error => {
+          console.log(error)
+        })
     },
     remove(id) {
-      console.log(id)
+      axios.delete(`employee/delete/${id}`)
+        .then(() => {
+          this.list()
+        })
+        .catch(error => {
+          console.log(error)
+        })
     }
   }
 } 
--- src/main.js
+++ src/main.js
@@ -5,12 +5,14 @@ import ElementUI from 'element-ui';
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import 'element-ui/lib/theme-chalk/index.css';
+import axios from 'axios'
 
 // Install BootstrapVue
 Vue.use(BootstrapVue)
 Vue.use(ElementUI);
 
 Vue.config.productionTip = false
+axios.defaults.baseURL = 'http://localhost:4000/api/'
 
 new Vue({
   render: h => h(App),
